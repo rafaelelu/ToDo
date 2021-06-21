621efa282b1f3badedda2bbba8648df5f86dbe0a
Rafael Elu <rafaelelu@Rafaels-MacBook-Pro.local>
2021-06-20 21:15:01 -0500

    Add Dexie to act as DB

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -1,41 +1,80 @@
-import List from "@material-ui/core/List";
+import { useState, useEffect } from 'react';
+import List from '@material-ui/core/List';
+import Button from '@material-ui/core/Button';
 import ToDoItem from './ToDoItem';
-
-const tasks = [
-    {
-        title: 'Sacar a pasear a Joe',
-        description: 'Ir al parque con Joe',
-        important: true,
-        urgent: true
-    },
-    {
-        title: 'Comprar leche',
-        description: 'Ir al súper y comprar leche',
-        important: true,
-        urgent: false
-    },
-    {
-        title: 'Arreglar mi cuarto',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-        important: false,
-        urgent: true
-    },
-    {
-        title: 'Otra tarea',
-        description: 'This is a test description',
-        important: false,
-        urgent: false
-    }
-];
-
-
+import DexieSingleton from '../data/DexieSingleton';
 
 export default function ToDoList() {
+    const [ tasks, setTasks ] = useState( [] );
+    const [ error, setError ] = useState( null );
+    const [ isLoading, setIsLoading ] = useState( true );
+    const db = DexieSingleton.getInstance();
+
+    /**
+     * Fetches the tasks from the local db
+     */
+    const fetchTasks = () => {
+        setIsLoading( true );
+        db.tasks.toArray()
+            .then( ( tasks ) => {
+                setTasks( tasks );
+            } )
+            .catch( ( error ) => {
+                setError( error );
+            } )
+            .finally( () => {
+                setIsLoading( false );
+            } );
+    };
+
+    /**
+     * 
+     * @param { Object } task - The task to be added
+     * @param { string } task.title - The task's title
+     * @param { string } task.description - The task's description
+     * @param { bool } task.important - If the task is important or not
+     * @param { bool } task.urgent - If the task is urgent or not
+     */
+    const addTask = ( task ) => {
+        db.tasks.add( task )
+            .then( () => {
+                fetchTasks();
+            } )
+            .catch( ( error ) => {
+                setError( error.message );
+            } );
+    };
+
+    
+    useEffect( () => {
+        fetchTasks();
+    }, [] );
+
     return(
-        <List>
-            { tasks.map( task => (
-                <ToDoItem task={ task } key={ task.title } />
-            ) ) }
-        </List>
+        <div>
+            <Button
+                variant="contained"
+                color="primary"
+                onClick={ () => {
+                    addTask( {
+                        title: 'Another task',
+                        description: 'Tak\'s description',
+                        important: false,
+                        urgent: false
+                    } );
+                } }
+            >
+                    New task
+            </Button>
+            { error && <p>{ error.message }</p> }
+            { isLoading && <p>Loading...</p> }
+            { tasks &&
+                <List>
+                    { tasks.map( task => (
+                        <ToDoItem task={ task } key={ task.title } />
+                    ) ) }
+                </List>
+            }
+        </div>
     );
 }
